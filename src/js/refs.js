449c6a937925625a189d7ba3c39b1f8c87352926
AlexSyrov <1854105@gmail.com>
2020-11-23 21:23:19 +0200

    комит №1

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    searchForm: document.querySelector('.form-input'),
+    countrieP: document.querySelector('.coun-p'),
+    form: document.querySelector('.list-countries')
+  };
+}
